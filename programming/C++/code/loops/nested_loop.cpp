b14a2b3f57fa83a97f3b9d388265bb8031aa7942
Vidath Dissanayake <dmvlayanga@gmail.com>
2026-01-11 09:17:14 +0530

    vault backup: 2026-01-11 09:17:13
    Affected files:
    programming/C++/code/loops/nested_loop.cpp
    programming/C++/code/loops/nested_loop.exe
    programming/C++/code/loops/nested_loop.ilk
    programming/C++/code/loops/nested_loop.obj
    programming/C++/code/loops/nested_loop.pdb
    programming/C++/code/loops/vc140.pdb

--- programming/C++/code/loops/nested_loop.cpp
+++ programming/C++/code/loops/nested_loop.cpp
@@ -2,4 +2,16 @@
 using namespace std;
 
 int main()
-{}
+{
+    int i;
+    int j;
+    for (i=1; i<=10;i++)
+    {
+        for (j=1;j<=i;j++)
+        {
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
